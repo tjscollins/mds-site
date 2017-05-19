0fef77cd566daa2d5e242217e6a70d0579bbb18e
Tyler Collins <tjscollins@gmail.com>
2017-05-19 18:01:39 +1000

    Setting up db for student data

--- resources/sql/student-queries.sql
+++ resources/sql/student-queries.sql
@@ -1,22 +1,21 @@
--- :name create-user! :! :n
+-- :name create-student! :! :n
 -- :doc creates a new user record
 INSERT INTO students
 (id, first_name, last_name, bio_blurb)
 VALUES (:id, :first_name, :last_name, :bio_blurb)
 
--- :name update-user! :! :n
+-- :name update-student! :! :n
 -- :doc update an existing user record
 UPDATE students
 SET first_name = :first_name, last_name = :last_name, bio_blurb=:bio_blurb
 WHERE id = :id
 
--- :name get-user :? :1
+-- :name get-students :? :1
 -- :doc retrieve a user given the id.
 SELECT * FROM students
 WHERE id = :id
 
--- :name delete-user! :! :n
+-- :name delete-student! :! :n
 -- :doc delete a user given the id
 DELETE FROM students
 WHERE id = :id
-

b0f58e3bb6d3cad091d39d5995fd1e0013b69d55
Tyler Collins <tjscollins@gmail.com>
2017-05-25 12:58:54 +1000

    Change user model

---
+++ resources/migrations/20170525121918-update-users-table.down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE users
+DROP username;
+ALTER TABLE users
+ADD first_name varchar (30);
+ALTER TABLE users
+ADD last_name varchar (30);
+ALTER TABLE users
+ADD email varchar (30);
---
+++ resources/migrations/20170525121918-update-users-table.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE users
+ADD username varchar (30);
+ALTER TABLE users
+DROP first_name;
+ALTER TABLE users
+DROP last_name;
+ALTER TABLE users
+DROP email;
